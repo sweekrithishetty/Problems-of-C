dc01eb53d96831cbe2b854ac3bc1c089a074ff4f
Sweekrithi  S  Shetty <shettysweekrithi03@gmail.com>
2021-02-18 06:07:40 -0500

    Add files via upload

---
+++ Fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,n,result,i;
+    printf("Enter the number of terms: ");
+    scanf("%d",&n);
+
+    a=0;
+    b=1;
+    for(i=1;i<=n;i++)
+    {
+        printf("%d", a);
+
+        result= a+b;
+        a=b;
+        b=result;
+
+    }
+
+}
